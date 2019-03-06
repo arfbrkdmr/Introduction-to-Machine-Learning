c1a80af5eb6e8855d10159054fb63a8dc3ba231a
Tech AJ <32822650+halanch599@users.noreply.github.com>
2019-03-06 17:24:38 +0300

    splitData function
    It splits the data into train and test datasets.
    You can call this function as following:
    p is the training data percentage: like 20% for training and 80% for testing
    [train_x,train_y, test_x, test_y] = splitData(data,0.20);

---
+++ splitData.m
@@ -0,0 +1,8 @@
+function [train_x,train_y, test_x, test_y] = splitData(data,p)
+    [m,n] = size(data);
+    index = randperm(m);
+    train_x= data(index(1:round(p*m)),1:n-1);
+    train_y= data(index(1:round(p*m)),1:n);
+    test_x = data(index(round(p*m)+1:end),1:n-1);
+    test_y = data(index(round(p*m)+1:end),1:n);
+end
