6c5238196bf7f9f51a6598f95faa07b3072aba1d
Tech AJ <32822650+halanch599@users.noreply.github.com>
2019-03-07 16:58:12 +0300

    Update splitData.m

--- splitData.m
+++ splitData.m
@@ -2,7 +2,7 @@ function [train_x,train_y, test_x, test_y] = splitData(data,p)
     [m,n] = size(data);
     index = randperm(m);
     train_x= data(index(1:round(p*m)),1:n-1);
-    train_y= data(index(1:round(p*m)),1:n);
+    train_y= data(index(1:round(p*m)),n);
     test_x = data(index(round(p*m)+1:end),1:n-1);
-    test_y = data(index(round(p*m)+1:end),1:n);
+    test_y = data(index(round(p*m)+1:end),n);
 end
